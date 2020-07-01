565b3dbce4b3f4db3af32927b9ff04a0aaa62afc
Jeremy Herve <jeremy@jeremy.hu>
2020-07-01 18:09:23 +0200

    Repo Gardening: add new action to mark issues being worked on
    When a PR is created to fix a specific issue, let's:
    1. Assign the issue to the PR author
    2. Mark the issue as In progress.

---
+++ .github/actions/assign-issues.js
@@ -0,0 +1,33 @@
+/*
+ * See https://github.com/octokit/action.js/ for more info
+ */
+const octokit = new Octokit();
+
+// Get info about the event.
+const eventPayload = require( process.env.GITHUB_EVENT_PATH );
+
+// Look for words indicating that a PR fixes an issue.
+const regex = /(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved):? +(?:\#?|https?:\/\/github\.com\/jeherve\/jetpack\/issues\/)(\d+)/gi;
+
+let match;
+while ( ( match = regex.exec( eventPayload.pull_request.body ) ) ) {
+	const [ , issue ] = match;
+
+	// Assign the issue to the PR author.
+	const addAssignee = await octokit.issues.addAssignees( {
+		owner: eventPayload.repository.owner.login,
+		repo: eventPayload.repository.name,
+		issue_number: +issue,
+		assignees: [ eventPayload.pull_request.user.login ],
+	} );
+	console.log( `PR author assigned: ${ addAssignee }` );
+
+	// Add the In Progress label to the issue.
+	const addLabel = await octokit.issues.addLabels( {
+		owner: eventPayload.repository.owner.login,
+		repo: eventPayload.repository.name,
+		issue_number: +issue,
+		labels: [ '[Status] In Progress' ],
+	} );
+	console.log( `Label added: ${ addLabel }` );
+}
