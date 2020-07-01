81e7ce3a71187459537c8f9e9d62cdbdd7e7952b
Jeremy Herve <jeremy@jeremy.hu>
2020-07-01 19:11:15 +0200

    Some cleanup

--- .github/actions/assign-issues/src/index.js
+++ .github/actions/assign-issues/src/index.js
@@ -5,8 +5,6 @@ const { setFailed, getInput } = require( '@actions/core' );
 const { context, GitHub } = require( '@actions/github' );
 
 async function run() {
-	console.log( `it's running!` );
-
 	const token = getInput( 'token' );
 	if ( ! token ) {
 		setFailed( 'main: Input `token` is required' );
@@ -17,7 +15,6 @@ async function run() {
 
 	// Get info about the event.
 	const eventPayload = context.payload;
-	console.log( `Our event payload is ${ eventPayload }` );
 
 	// Look for words indicating that a PR fixes an issue.
 	const regex = /(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved):? +(?:\#?|https?:\/\/github\.com\/jeherve\/jetpack\/issues\/)(\d+)/gi;
@@ -28,22 +25,20 @@ async function run() {
 		console.log( `We have a match. The issue ID is ${ issue }` );
 
 		// Assign the issue to the PR author.
-		const addAssignee = await octokit.issues.addAssignees( {
+		await octokit.issues.addAssignees( {
 			owner: eventPayload.repository.owner.login,
 			repo: eventPayload.repository.name,
 			issue_number: +issue,
 			assignees: [ eventPayload.pull_request.user.login ],
 		} );
-		console.log( `PR author assigned: ${ addAssignee }` );
 
 		// Add the In Progress label to the issue.
-		const addLabel = await octokit.issues.addLabels( {
+		await octokit.issues.addLabels( {
 			owner: eventPayload.repository.owner.login,
 			repo: eventPayload.repository.name,
 			issue_number: +issue,
 			labels: [ '[Status] In Progress' ],
 		} );
-		console.log( `Label added: ${ addLabel }` );
 	}
 }
 
