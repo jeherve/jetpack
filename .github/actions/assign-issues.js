9584c39d1eed24434a44473a389debedd9670448
Jeremy Herve <jeremy@jeremy.hu>
2020-07-01 18:34:57 +0200

    More debug info

--- .github/actions/assign-issues.js
+++ .github/actions/assign-issues.js
@@ -6,13 +6,14 @@ async function run() {
 
 	// Get info about the event.
 	const eventPayload = require( process.env.GITHUB_EVENT_PATH );
-
+	console.log( `Our event payload is ${ eventPayload }` );
 	// Look for words indicating that a PR fixes an issue.
 	const regex = /(?:close|closes|closed|fix|fixes|fixed|resolve|resolves|resolved):? +(?:\#?|https?:\/\/github\.com\/jeherve\/jetpack\/issues\/)(\d+)/gi;
 
 	let match;
 	while ( ( match = regex.exec( eventPayload.pull_request.body ) ) ) {
 		const [ , issue ] = match;
+		console.log( `We have a match. The issue ID is ${ issue }` );
 
 		// Assign the issue to the PR author.
 		const addAssignee = await octokit.issues.addAssignees( {
